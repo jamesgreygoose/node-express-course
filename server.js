498800b5e715c6e57e97e7e75238312e49a109d1
jamesgreygoose <james@schoolofcode.co.uk>
2020-08-09 15:25:02 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const app=express()
+const bodyParser= require("body-parser")
 const PORT=8000
 const mockUserData=require("./names.json")
 
+app.use(bodyParser.json());
 
     app.get('/users', function(req,res){
          res.json({
@@ -21,6 +23,27 @@ const mockUserData=require("./names.json")
         })
     })
 
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
+})
 
 
 
